6713876c42fee8aaae27d5325d219107f0c35d7b
ShaiLevi23 <shailevi23@gmail.com>
2022-11-02 11:46:43 +0200

    infratrature

---
+++ StructrualPatterns/src/main/java/org/gfg/HttpFacade.java
@@ -0,0 +1,12 @@
+package org.gfg;
+
+public class HttpFacade {
+
+    //decouple client from apache http
+    //Facade does alot of actions and the client just calls a method.
+
+    public void getRequest(){}
+    public void postRequest(){}
+    public void putRequest(){}
+    public void deleteRequest(){}
+}
